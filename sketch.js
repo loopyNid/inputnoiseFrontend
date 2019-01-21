7234388db69bde77398f4ae229937c272b94dd0b
nikk charalampidis <nikk.ch@protonmail.com>
2019-01-21 21:29:02 +0200

    initial commit

---
+++ sketch.js
@@ -0,0 +1,26 @@
+var source;
+var fft;
+const flock = [];
+console.log("i am alive");
+function setup() {
+    var audioCtx = getAudioContext();
+    var myMediaElement = document.getElementById('my-stream');
+    var source = audioCtx.createMediaElementSource(myMediaElement);
+    source.connect(p5.soundOut);
+    fft = new p5.FFT(0, 1024);
+    source.connect(audioCtx.destination);
+    createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+    background(0);
+    stroke(0, 87, 87);
+    fill(0);
+    var spectrum = fft.analyze();
+    for(var i = 0; i < spectrum.length; i++){
+        index = map(i, 0, 1024, 0, 1);
+        strokeWeight(15 * index);
+        ampFreq = map(spectrum[i], 0, 255, 0, 1);
+        ellipse(width * index,random(height),100 * ampFreq, 100 * ampFreq);
+    }
+}
